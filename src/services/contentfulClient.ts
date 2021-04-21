e6ea20369c5350fa8437188c422aa64a0a39a743
Damian Balas <damian.webdev@gmail.com>
2021-04-21 19:10:58 +0200

    add contentfulClient service

---
+++ src/services/contentfulClient.ts
@@ -0,0 +1,21 @@
+import { createClient } from "contentful";
+
+const contentfulClient = () => {
+  if (
+    !process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID ||
+    !process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+  ) {
+    throw new Error(
+      "No space or access token for contentful set! Please check your .env file.",
+    );
+  }
+
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  return client;
+};
+
+export default contentfulClient();
